1ab8209850467886628bdd924fba42083b3e91aa
Amr Abd ElHamed <amr2652001@gmail.com>
2025-03-27 05:44:56 +0200

    solve navigate to realestate screen

--- lib/features/main_services_layout/app_services.dart
+++ lib/features/main_services_layout/app_services.dart
@@ -1,7 +1,9 @@
+import 'package:easy_localization/easy_localization.dart';
+import '../../core/translation/locale_keys.dart';
 import '../home_module/home_imports.dart';
 import '../real_estates/presentation/screens/real_estates_screen.dart';
 
 final Map<String, Widget Function(BuildContext)> appServiceScreens = {
-  'عقارات': (context) => const RealStateScreen(),
+  LocaleKeys.realEstate.tr(): (context) => const RealStateScreen(),
 
 };
